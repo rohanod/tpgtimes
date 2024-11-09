74b3e0628c5acaa6fa524816fdc1748e5b85ddcd
Rohan O'Dwyer <166503624+rohanod@users.noreply.github.com>
2024-11-09 09:33:25 +0100

    Initial Commit

---
+++ tpgtimes/tpgtimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  tpgtimesApp.swift
+//  tpgtimes
+//
+//  Created by Rohan O'Dwyer on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct tpgtimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tpgtimesTests/tpgtimesTests.swift
@@ -0,0 +1,17 @@
+//
+//  tpgtimesTests.swift
+//  tpgtimesTests
+//
+//  Created by Rohan O'Dwyer on 11/9/24.
+//
+
+import Testing
+@testable import tpgtimes
+
+struct tpgtimesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
